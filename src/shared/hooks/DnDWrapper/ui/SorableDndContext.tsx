1e78b6dcbc022088010684a60ca1b11ed1cfb32c
Jin SeungHyeon <jinsh9050@gmail.com>
2025-04-14 08:38:55 +0900

    SorableDnDContext disable prop add

--- src/shared/hooks/DnDWrapper/ui/SorableDndContext.tsx
+++ src/shared/hooks/DnDWrapper/ui/SorableDndContext.tsx
@@ -12,12 +12,14 @@ type SortableDndContextProps<T extends Id> = {
   children: (items: T[]) => React.ReactNode;
   handleDragEnd: (newItems: T[], active: T) => void;
   data: T[];
+  disabled?: boolean;
 };
 
 const SortableDndContext = <T extends Id>({
   children,
   handleDragEnd,
   data,
+  disabled = false,
 }: SortableDndContextProps<T>) => {
   const { items, onDragEnd } = useDnD<T>(data || []);
   const sensors = useSensors(
@@ -32,7 +34,7 @@ const SortableDndContext = <T extends Id>({
       onDragEnd={onDragEnd(handleDragEnd)}
       sensors={sensors}
     >
-      <SortableContext items={items}>
+      <SortableContext items={items} disabled={disabled}>
         {children(items || [])}
       </SortableContext>
     </DndContext>
